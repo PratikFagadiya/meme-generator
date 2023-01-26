088392d3c8b202d31d541de2416e53520f742cf7
Pratik Fagadiya <pratikfagadiya@gmail.com>
2023-01-26 15:15:40 +0530

    Api calling

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -4,31 +4,53 @@ import fetchData from "../network/FetchData";
 
 export default function Meme() {
 
-    const [memeData, setMemeData] = useState()
+    const [memeData, setMemeData] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         const getDataFromApi = async () => {
-            const data = await fetchData('get_memes')
-            if (data.success) {
-                setMemeData(data.data.memes[0].name)
+            try {
+                const data = await fetchData('get_memes')
+                if (data.success) {
+                    setMemeData(data.data.memes[0].name)
+                    setError(null)
+                }
+            } catch (e) {
+                setMemeData(null)
+                setError(e.message)
+            } finally {
+                setLoading(false)
             }
+
         }
         getDataFromApi();
+
+        // setTimeout(() => {
+        //     setLoading(false)
+        // },2000)
+
     }, [])
 
-    return (
-        <main>
-            <form className={'form'}>
-                <section className={'inputs'}>
-                    <input className={'form-input'} type={"text"} placeholder={'Top Text'}/>
-                    <input className={'form-input'} type={"text"} placeholder={'Bottom Text'}/>
-                </section>
+    return (<main>
 
-                <button className={'button-submit'} onClick={submitButtonClicked}>Get a new meme image</button>
+        <form className={'form'}>
 
-            </form>
-        </main>
-    )
+            <section className={'inputs'}>
+                <input className={'form-input'} type={"text"} placeholder={'Top Text'}/>
+                <input className={'form-input'} type={"text"} placeholder={'Bottom Text'}/>
+            </section>
+
+            <button className={'button-submit'} onClick={submitButtonClicked}>Get a new meme image</button>
+
+        </form>
+
+        {loading && <span>Generating Memes for you... </span>}
+        {error && <span>`There is a problem ${error}`</span>}
+        {/*{memeData &&}*/}
+
+
+    </main>)
 
     function submitButtonClicked() {
         console.log("I Was clicked")
